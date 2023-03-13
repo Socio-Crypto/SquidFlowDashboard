56d5cfd8932857286dae86d502547475d71ac215
zahra.bahrami <z.bahrami@danalytica.com>
2023-03-14 00:59:13 +0330

    style of page changed

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
 from django.views.generic import View
+
+from .services import get_wallet_activity
 
 class DashboardView(View):
 
     def get(self, request):
         context = {}
-        
+        # a = get_wallet_activity()
         return render(request, 'dashboard.html', context=context)
