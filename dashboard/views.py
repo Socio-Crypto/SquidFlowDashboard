1bc2824167dacad59b122149199f0edfd9b207f4
zahra.bahrami <z.bahrami@danalytica.com>
2023-03-13 23:11:49 +0330

    redundant files deleted

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.views.generic import View
+
+class DashboardView(View):
+
+    def get(self, request):
+        context = {}
+        
+        return render(request, 'dashboard.html', context=context)
