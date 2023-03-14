571730ab1e33d9e8601c417c645722e3a05dc2d8
zahra.bahrami <z.bahrami@danalytica.com>
2023-03-14 16:07:57 +0330

    setting updated

--- SquidFlow/settings.py
+++ SquidFlow/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -116,6 +116,26 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_URL = "/static/"
+MEDIA_URL = "/images/"
+
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/")]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+
+
 
 STATIC_URL = 'static/'
 
