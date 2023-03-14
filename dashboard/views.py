a587ccc60d45145693aa858ba74a280614a34250
zahra.bahrami <z.bahrami@danalytica.com>
2023-03-14 15:40:36 +0330

    new django project created

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,3 @@
 from django.shortcuts import render
-from django.views.generic import View
 
-from .services import get_wallet_activity
-
-class DashboardView(View):
-
-    def get(self, request):
-        context = {}
-        # a = get_wallet_activity()
-        return render(request, 'dashboard.html', context=context)
+# Create your views here.
