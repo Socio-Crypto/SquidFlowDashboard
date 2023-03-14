b9cd6a8a671fd5047eee72ffacca54c72ee73570
zahra.bahrami <z.bahrami@danalytica.com>
2023-03-14 15:27:37 +0330

    static path changed

--- SquidFlowDashboard/settings.py
+++ SquidFlowDashboard/settings.py
@@ -146,3 +146,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = 'static/'
